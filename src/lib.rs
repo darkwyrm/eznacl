0f8bb82755a53d864db05e2e6a42b6cd469654d5
Jon Yoder <jyoder@bcadmin.com>
2022-02-14 11:30:40 -0500

    Convert spaces to tabs

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,9 @@ extern crate lazy_static;
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+	#[test]
+	fn it_works() {
+		let result = 2 + 2;
+		assert_eq!(result, 4);
+	}
 }
