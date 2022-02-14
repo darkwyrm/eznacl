e3cd1522d181c4364a0bfe27d50cffa63ddb4b3f
Jon Yoder <jyoder@bcadmin.com>
2022-02-14 14:22:32 -0500

    Remove boilerplate test

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,3 @@ pub use cryptostring::CryptoString;
 
 #[macro_use]
 extern crate lazy_static;
-
-#[cfg(test)]
-mod tests {
-	#[test]
-	fn it_works() {
-		let result = 2 + 2;
-		assert_eq!(result, 4);
-	}
-}
