e268a56d0b0e5e9d9f379f4cc2244e5a51b286e5
Jon Yoder <jon@yoder.cloud>
2022-02-20 14:33:15 -0500

    Add modules for signing and encryption

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,18 @@
 mod base;
 mod cryptostring;
+mod encrypt;
 mod error;
 mod hash;
 mod password;
+mod sign;
 
 pub use base::*;
 pub use cryptostring::CryptoString;
+pub use encrypt::*;
 pub use error::EzNaclError;
 pub use hash::*;
 pub use password::*;
+pub use sign::*;
 
 #[macro_use]
 extern crate lazy_static;
