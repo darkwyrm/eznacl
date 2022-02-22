6f917fc971dbdbaad00e9fded2d2a163289342eb
Jon Yoder <jyoder@bcadmin.com>
2022-02-22 10:27:24 -0500

    Fix return type for Encrypt, Decrypt, and Sign traits

--- src/base.rs
+++ src/base.rs
@@ -32,17 +32,17 @@ pub trait PrivateKey {
 
 /// The Encryptor trait is implemented by any encryption key
 pub trait Encryptor {
-	fn encrypt(self, data: &[u8]) -> CryptoString;
+	fn encrypt(self, data: &[u8]) -> Option<CryptoString>;
 }
 
 /// The Decryptor trait is implemented by any decryption key
 pub trait Decryptor {
-	fn decrypt(self, encdata: &str) -> Vec<u8>;
+	fn decrypt(self, encdata: &str) -> Option<Vec<u8>>;
 }
 
 /// The Sign trait is implemented by any private signing key
 pub trait Sign {
-	fn sign(self, data: &[u8]) -> CryptoString;
+	fn sign(self, data: &[u8]) -> Option<CryptoString>;
 }
 
 /// The Verify trait is implemented by any public signature verification key
