df6e2917cdae2ab7eada6bf721f5d81e07a72a53
Jon Yoder <jyoder@bcadmin.com>
2022-02-23 12:45:29 -0500

    Implement Encrypt and Decrypt for EncryptionPair

--- src/encrypt.rs
+++ src/encrypt.rs
@@ -1,6 +1,7 @@
 use crate::CryptoString;
-use crate::base::{CryptoInfo, PublicKey, PrivateKey, KeyUsage};
-use sodiumoxide;
+use crate::base::{CryptoInfo, PublicKey, PrivateKey, KeyUsage, Encryptor, Decryptor};
+use sodiumoxide::crypto;
+use crate::error::EzNaclError;
 
 /// A Curve25519 asymmetric encryption keypair
 pub struct EncryptionPair {
@@ -16,7 +17,7 @@ impl EncryptionPair {
 
 	/// Generates a Curve25519 asymmetric encryption keypair.
 	pub fn generate() -> Option<EncryptionPair> {
-		let (raw_ekey, raw_dkey) = sodiumoxide::crypto::box_::gen_keypair();
+		let (raw_ekey, raw_dkey) = crypto::box_::gen_keypair();
 		let pubkey = CryptoString::from_bytes("CURVE25519", &raw_ekey[..])?;
 
 		let privkey = CryptoString::from_bytes("CURVE25519", &raw_dkey[..])?;
@@ -64,3 +65,42 @@ impl PrivateKey for EncryptionPair {
 		Vec::from(self.privkey.as_bytes())
 	}
 }
+
+impl Encryptor for EncryptionPair {
+	
+	fn encrypt(self, data: &[u8]) -> Result<CryptoString, EzNaclError> {
+
+		let rawkey = match crypto::box_::PublicKey::from_slice(self.pubkey.as_bytes()) {
+			Some(v) => v,
+			None => return Err(EzNaclError::KeyError)
+		};
+	
+		let ciphertext = crypto::sealedbox::seal(data, &rawkey);
+		match CryptoString::from_bytes("CURVE25519", &ciphertext) {
+			Some(v) => Ok(v),
+			None => Err(EzNaclError::EncodingError)
+		}
+	}
+}
+
+impl Decryptor for EncryptionPair {
+
+	fn decrypt(self, encdata: &CryptoString) -> Result<Vec<u8>, crate::EzNaclError> {
+
+		let ciphertext = encdata.as_raw();
+		let rpubkey = match crypto::box_::PublicKey::from_slice(self.pubkey.as_bytes()) {
+			Some(v) => v,
+			None => return Err(EzNaclError::KeyError)
+		};
+		let rprivkey = match crypto::box_::SecretKey::from_slice(self.privkey.as_bytes()) {
+			Some(v) => v,
+			None => return Err(EzNaclError::KeyError)
+		};
+
+		match crypto::sealedbox::open(&ciphertext, &rpubkey, &rprivkey) {
+			Ok(v) => Ok(v),
+			_ => Err(EzNaclError::DecryptionError)
+		}
+	}
+}
+
